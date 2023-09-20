c2d5dbe3fdcbf967fd2636085cde4fa6b999ab3c
sishva12 <145535839+sishva12@users.noreply.github.com>
2023-09-20 12:46:12 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import os
 #os.mkdir("C:\sishva")
 #sishFD=open("C:\sishva\\DURGA.txt","x")
 #os.mkdir("C:\pawan")
-pawanFD=open("C:\pawan\\kiran.txt","x")
-pawanFD.write("kiran is absent")
-print(pawanFD)
+#pawanFD=open("C:\pawan\\kiran.txt","a")
+#pawanFD.write("kiran is absent")
+#print(pawanFD)
+#pawanFD=open("C:\pawan\\kiran.txt1","a+")
+#pawanFD.write("kiran is absent")
+#print(pawanFD)
+
 
 
 
